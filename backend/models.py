15c1710aa2317c3d011cc93eca7ccd5837dd1b01
unknown <b.dipakkumarxe@gmail.com>
2026-02-08 22:44:00 +0530

    fronend farmer authentication

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=True)
-    mobile = Column(String, unique=True, index=True)
-    password = Column(String)
-    role = Column(String)   # farmer or consumer
+
+    name = Column(String, nullable=False)
+    mobile = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=True)
+
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False)   # farmer / consumer
+
+    state = Column(String, nullable=True)
+    district = Column(String, nullable=True)
+    village = Column(String, nullable=True)
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,11 +1,15 @@
 from pydantic import BaseModel
 
 class RegisterUser(BaseModel):
+    name: str
     mobile: str
+    email: str
     password: str
     role: str
+    state: str
+    district: str
+    village: str
 
 class LoginUser(BaseModel):
     mobile: str
     password: str
-
